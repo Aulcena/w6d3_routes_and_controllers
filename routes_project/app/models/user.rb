a15b065abaee8b954f47a3c462a22065fa22e548
AJU <aulcena@gmail.com>
2023-08-15 03:11:51 -0400

    changed users table

--- routes_project/app/models/user.rb
+++ routes_project/app/models/user.rb
@@ -3,14 +3,13 @@
 # Table name: users
 #
 #  id         :bigint           not null, primary key
-#  name       :string
-#  email      :string
+#  username   :string           not null
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 class User < ApplicationRecord
 
-    validates :name, :email, presence: true
+    validates :username, presence: true, uniqueness: true
 
     has_one :user,
     primary_key: :id,
